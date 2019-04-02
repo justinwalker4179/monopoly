467be7b92101b5783c299f1a02d11dad27e59835
Justin Walker <jrwalker314@yahoo.com>
2019-04-01 19:03:20 -0700

    Initial commit, game skeleton.

---
+++ monopoly.py
@@ -0,0 +1,68 @@
+import random
+
+class Monopoly:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def check_win(self):
+        pass
+
+    def take_turn(self):
+        pass
+    def roll_and_move(self):
+        pass
+    
+    def buy_property(self):
+        pass
+    def auction_property(self):
+        pass
+
+    def go_to_jail(self):
+        pass
+
+    def draw_chance_card(self):
+        pass
+    def draw_community_card(self):
+        pass
+
+game_board = [
+    [ [],[],[],[],[],[],[],[],[],[],[] ],
+    [ [],                           [] ],
+    [ [],                           [] ],
+    [ [],                           [] ],
+    [ [],                           [] ],
+    [ [],                           [] ],
+    [ [],                           [] ],
+    [ [],                           [] ],
+    [ [],                           [] ],
+    [ [],                           [] ],
+    [ [],[],[],[],[],[],[],[],[],[],[] ]
+]
+
+chance_deck =
+    [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+    ]
+
+community_deck =
+    [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+    ]
